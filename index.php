9ee237fed2974a83f75780eac084def66829da46
Julius Alvarado <javascript.uiux@gmail.com>
2018-09-10 19:54:41 -0700

    wrote 1st script to query BigQuery
    this only works in powershell

--- index.php
+++ index.php
@@ -2,8 +2,41 @@
 
 use Google\Cloud\BigQuery\BigQueryClient;
 
+
+require __DIR__ . '\vendor\autoload.php';
 require __DIR__ . '\config\info.php';
 
 $bigQuery = new BigQueryClient([
-    'projectId' => RSM_PROJECT_ID
+    'projectId' => $argv[1]
 ]);
+
+$q = <<<NinjaSQL
+SELECT CONCAT(
+      'https://stackoverflow.com/questions/',
+      CAST(id AS STRING)
+    ) AS url
+FROM `bigquery-public-data.stackoverflow.posts_questions`
+WHERE tags LIKE '%google-bigquery%'
+ORDER BY view_count DESC
+LIMIT 10
+NinjaSQL;
+
+$qJobConfig = $bigQuery->query($q);
+$qResults = $bigQuery->runQuery($qJobConfig);
+
+if($qResults->isComplete()) {
+    $i = 0;
+    $rows = $qResults->rows();
+    foreach($rows as $row) {
+        echo "\njha - next row:\n";
+        printf('- ROW: %s', PHP_EOL, ++$i);
+        printf('url: %s, views: $s', PHP_EOL, $row['url'], $row['view_count']);
+    }
+    printf('TOTAL ROWS: %s', PHP_EOL, $i);
+} else {
+    throw new Exception('ERROR - query failed');
+}
+
+
+
+echo RSM_PROJECT_ID . " query complete.";
---
+++ stackoverflow-sample.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julius
+ * Date: 9/10/2018
+ * Time: 7:06 PM
+ */
+
+/**
+ * For instructions on how to run the full sample:
+ *
+ * @see https://github.com/GoogleCloudPlatform/php-docs-samples/tree/master/bigquery/api/README.md
+ */
+
+# [START bigquery_simple_app_all]
+require __DIR__ . '/vendor/autoload.php';
+
+# [START bigquery_simple_app_deps]
+use Google\Cloud\BigQuery\BigQueryClient;
+
+# [END bigquery_simple_app_deps]
+
+// get the project ID as the first argument
+if (2 != count($argv)) {
+    die("Usage: php stackoverflow.php YOUR_PROJECT_ID\n");
+}
+
+$projectId = $argv[1];
+
+# [START bigquery_simple_app_client]
+$bigQuery = new BigQueryClient([
+    'projectId' => $projectId,
+]);
+# [END bigquery_simple_app_client]
+# [START bigquery_simple_app_query]
+$query = <<<ENDSQL
+SELECT
+  CONCAT(
+    'https://stackoverflow.com/questions/',
+    CAST(id as STRING)) as url,
+  view_count
+FROM `bigquery-public-data.stackoverflow.posts_questions`
+WHERE tags like '%google-bigquery%'
+ORDER BY view_count DESC
+LIMIT 10;
+ENDSQL;
+$queryJobConfig = $bigQuery->query($query);
+$queryResults = $bigQuery->runQuery($queryJobConfig);
+# [END bigquery_simple_app_query]
+
+# [START bigquery_simple_app_print]
+if ($queryResults->isComplete()) {
+    $i = 0;
+    $rows = $queryResults->rows();
+    foreach ($rows as $row) {
+        printf('--- Row %s ---' . PHP_EOL, ++$i);
+        printf('url: %s, %s views' . PHP_EOL, $row['url'], $row['view_count']);
+    }
+    printf('Found %s row(s)' . PHP_EOL, $i);
+} else {
+    throw new Exception('The query failed to complete');
+}
+# [END bigquery_simple_app_print]
+# [END bigquery_simple_app_all]
